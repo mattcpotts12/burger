c9985ffd14a8815274a7e3980ac9c2e80e750638
Potts <MPotts@balfourbeattyus.com>
2018-02-14 16:18:00 -0600

    -

---
+++ controllers/burger_controller.js
@@ -0,0 +1,45 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+//Routes and logic
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function(result) {
+        res.redirect("/")
+    });
+});
+
+router.put("/burger/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function(result) {
+        res.redirect("/")
+    });
+});
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", false);
+
